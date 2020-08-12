0517ef48e3b231bf48c6c796c453d9a1ef752a0d
David Manthey <david.manthey@kitware.com>
2020-08-12 09:42:58 -0400

    Allow for redaction replacement values.
    Strikethrough redacted values.
    
    Adjust which redact buttons are shown.  Hide some fields.

--- dsa_seer/web_client/views/itemView.js
+++ dsa_seer/web_client/views/itemView.js
@@ -11,8 +11,8 @@ wrap(ItemView, 'render', function (render) {
 
     const getRedactList = () => {
         let redact_list = (this.model.get('meta') || {}).redact_list || {};
-        redact_list.metadata = redact_list.metadata || [];
-        redact_list.images = redact_list.images || [];
+        redact_list.metadata = redact_list.metadata || {};
+        redact_list.images = redact_list.images || {};
         return redact_list;
     };
 
@@ -23,18 +23,39 @@ wrap(ItemView, 'render', function (render) {
         const category = target.attr('category');
         const undo = target.hasClass('undo');
         const redact_list = getRedactList();
-        const index = redact_list[category].indexOf(keyname);
-        if (index >= 0 && undo) {
-            redact_list[category].splice(index, 1);
-        } else if (index < 0 && !undo) {
-            redact_list[category].push(keyname);
+        let is_redacted = redact_list[category][keyname] !== undefined;
+        if (is_redacted && undo) {
+            delete redact_list[category][keyname];
+        } else if (!is_redacted && !undo) {
+            redact_list[category][keyname] = null;
         }
         this.model.editMetadata('redact_list', 'redact_list', redact_list);
-        target.toggleClass('undo');
         if (this.model.get('meta') === undefined) {
             this.model.set('meta', {});
         }
         this.model.get('meta').redact_list = redact_list;
+        is_redacted = !is_redacted;
+        target.toggleClass('undo');
+        target.closest('td').toggleClass('redacted', is_redacted);
+        target.closest('td').find('.redact-replacement').remove();
+        return false;
+    };
+
+    const showRedactButton = (keyname) => {
+        if (keyname.match(/^internal;openslide;openslide\.(?!comment$)/)) {
+            return false;
+        }
+        if (keyname.match(/^internal;openslide;tiff.ResolutionUnit$/)) {
+            return false;
+        }
+        return true;
+    };
+
+    const hideField = (keyname) => {
+        const is_aperio = this.$el.find('.large_image_metadata_value[keyname="internal;openslide;aperio.Title"]').length > 0;
+        if (is_aperio && keyname.match(/^internal;openslide;(openslide.comment|tiff.ImageDescription)$/)) {
+            return true;
+        }
         return false;
     };
 
@@ -53,19 +74,28 @@ wrap(ItemView, 'render', function (render) {
             if (!keyname || ['internal;tilesource'].indexOf(keyname) >= 0) {
                 return;
             }
-            let is_redacted = redact_list.metadata.indexOf(keyname) >= 0;
+            let is_redacted = redact_list.metadata[keyname] !== undefined;
             elem.find('.g-hui-redact').remove();
-            elem.append($('<a class="g-hui-redact' + (is_redacted ? ' undo' : '') + '"><span>Redact</span></a>').attr({
-                keyname: keyname,
-                category: 'metadata',
-                title: 'Toggle redacting this metadata'
-            }));
+            if (redact_list.metadata[keyname]) {
+                elem.append($('<span class="redact-replacement"/>').text(redact_list.metadata[keyname]));
+            }
+            if (showRedactButton(keyname)) {
+                elem.append($('<a class="g-hui-redact' + (is_redacted ? ' undo' : '') + '"><span>Redact</span></a>').attr({
+                    keyname: keyname,
+                    category: 'metadata',
+                    title: 'Toggle redacting this metadata'
+                }));
+            }
+            if (hideField(keyname)) {
+                elem.closest('tr').css('display', 'none');
+            }
+            elem.toggleClass('redacted', is_redacted);
         });
         // Add redaction controls to images
         this.$el.find('.g-widget-metadata-container.auximage .g-widget-auximage').each((idx, elem) => {
             elem = $(elem);
             let keyname = elem.attr('auximage');
-            let is_redacted = redact_list.images.indexOf(keyname) >= 0;
+            let is_redacted = redact_list.images[keyname] !== undefined;
             elem.find('.g-hui-redact').remove();
             elem.find('.g-widget-auximage-title').append($('<a class="g-hui-redact' + (is_redacted ? ' undo' : '') + '"><span>Redact</span></a>').attr({
                 keyname: keyname,
