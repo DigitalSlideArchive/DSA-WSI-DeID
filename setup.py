5bf2755a88aba1a5f85e5e6b47418ab429b212c6
David Manthey <david.manthey@kitware.com>
2020-08-14 12:09:25 -0400

    Use scm versioning.

--- setup.py
+++ setup.py
@@ -1,9 +1,31 @@
+import os
 from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
+
+def prerelease_local_scheme(version):
+    """
+    Return local scheme version unless building on master in CircleCI.
+
+    This function returns the local scheme version number
+    (e.g. 0.0.0.dev<N>+g<HASH>) unless building on CircleCI for a
+    pre-release in which case it ignores the hash and produces a
+    PEP440 compliant pre-release version number (e.g. 0.0.0.dev<N>).
+    """
+    from setuptools_scm.version import get_local_node_and_date
+
+    if os.getenv('CIRCLE_BRANCH') in ('master', ):
+        return ''
+    else:
+        return get_local_node_and_date(version)
+
+
 setup(
+    name='nci_seer',
+    use_scm_version={'local_scheme': prerelease_local_scheme},
+    setup_requires=['setuptools-scm'],
     author='Kitware, Inc.',
     author_email='kitware@kitware.com',
     classifiers=[
@@ -25,10 +47,8 @@ setup(
     long_description_content_type='text/x-rst',
     include_package_data=True,
     keywords='girder-plugin, nci_seer',
-    name='nci_seer',
     packages=find_packages(exclude=['test', 'test.*']),
     url='https://github.com/DigitalSlideArchive',
-    version='0.1.0',
     zip_safe=False,
     entry_points={
         'girder.plugin': [
