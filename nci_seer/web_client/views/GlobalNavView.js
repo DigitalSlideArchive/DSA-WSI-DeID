81d66fe796dc0199734fa2a47705b0c79f095042
David Manthey <david.manthey@kitware.com>
2020-08-26 11:58:45 -0400

    Add a global nav control for next unprocessed item.

---
+++ nci_seer/web_client/views/GlobalNavView.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+
+import { wrap } from '@girder/core/utilities/PluginUtils';
+import LayoutGlobalNavView from '@girder/core/views/layout/GlobalNavView';
+
+import { goToNextUnprocessedItem } from '../utils';
+import '../stylesheets/GlobalNavView.styl';
+
+wrap(LayoutGlobalNavView, 'render', function (render) {
+    render.call(this);
+
+    this.$el.find('ul.g-global-nav').append($(
+        '<li class="g-global-nav-li" title="Go to next unprocessed item">' +
+        '  <a class="g-nav-extra g-nav-next-unprocessed">' +
+        '    <i class="icon-right-big"></i><span>Next Item</span>' +
+        '  </a>' +
+        '</li>'));
+    this.events['click .g-nav-next-unprocessed'] = goToNextUnprocessedItem;
+    this.delegateEvents();
+    return this;
+});
